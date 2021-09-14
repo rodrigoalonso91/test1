b8e90a7590ca79bd47c211fb22c4f87bfca41629
Rodrigo Alonso <rodrigoalonso.hr@gmail.com>
2021-09-13 23:24:17 -0300

    Agregar archivos de proyecto.

---
+++ Post.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace test1
+{
+    public class Post
+    {
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+        [JsonPropertyName("body")]
+        public string Body { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            var url = "http://jsonplaceholder.typicode.com/posts";
+            await ProcessUrl(url, id: 1);
+        }
+
+        private static async Task ProcessUrl(string url, int id)
+        {
+            id--;
+            var httpResponse = await client.GetAsync(url);
+            var status = httpResponse.StatusCode;
+            var content = await httpResponse.Content.ReadAsStringAsync();
+            var post = JsonSerializer.Deserialize<List<Post>>(content);
+            var msg = $"Status: {status}\n\n" +
+                $"Content:\n" +
+                $"UserId: {post[id].UserId}\n" +
+                $"Id: {post[id].Id}\n" +
+                $"Title: {post[id].Title}\n" +
+                $"Body {post[id].Body}\n\n" +
+                $"Body Length: {post[id].Body.Length} characters";
+
+            Console.WriteLine(msg);
+        }
+    }
+}
